eba87365a1f0ec6c14d23207e7c23ff3a68a902a
shaheerqureshi <shaheerqureshi334@gmail.com>
2026-05-09 16:49:52 -0700

    Project 07 — Add VNet and subnets to Terraform config

--- project-07-iac-terraform-bicep/terraform/main.tf
+++ project-07-iac-terraform-bicep/terraform/main.tf
@@ -14,3 +14,24 @@ resource "azurerm_resource_group" "lab" {
   name = "rg-terraform-lab"
   location = "canadacentral"
 }
+
+resource "azurerm_virtual_network" "lab" {
+  name = "vnet-terraform-lab"
+  resource_group_name = azurerm_resource_group.lab.name
+  location = azurerm_resource_group.lab.location
+  address_space = [ "10.10.0.0/16" ]
+  
+}
+
+resource "azurerm_subnet" "web" {
+  name = "snet-web"
+  resource_group_name = azurerm_resource_group.lab.name
+  virtual_network_name = azurerm_virtual_network.lab.name
+  address_prefixes = [ "10.10.1.0/24" ]
+}
+resource "azurerm_subnet" "data" {
+  name = "snet-data"
+  resource_group_name = azurerm_resource_group.lab.name
+  virtual_network_name = azurerm_virtual_network.lab.name
+  address_prefixes = [ "10.10.2.0/24" ]
+}
